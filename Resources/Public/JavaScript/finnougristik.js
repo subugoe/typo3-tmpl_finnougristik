29265caad81ff285321e40ffa443b19097b00cfa
Hans-Werner Hilse <hwhilse@gmail.com>
2022-07-25 14:12:35 +0200

    fix that pesky not-working menu button in small-width mode

--- Resources/Public/JavaScript/finnougristik.js
+++ Resources/Public/JavaScript/finnougristik.js
@@ -1,6 +1,8 @@
 (function($) {
+ $(document).ready(function() {
   $('.menu-trigger').on('click', function () {
     $('.content-container').toggleClass('is-open');
     $(this).toggleClass('active');
   });
+ });
 })(jQuery);
